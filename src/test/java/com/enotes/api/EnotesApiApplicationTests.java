a9df0ffb49a75ad67a0324de756712329f859e33
bitturohit <rohit97750@gmail.com>
2025-07-21 15:11:39 +0530

    initial

---
+++ src/test/java/com/enotes/api/EnotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enotes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
